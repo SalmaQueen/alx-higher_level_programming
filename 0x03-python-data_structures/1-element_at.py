7c5b35210df0928f90687818ba8944db29304b92
salmaQueen <salmawasee9@gmail.com>
2023-08-16 22:12:19 +0300

    updated today's task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,3 +3,8 @@ def element_at(my_list, idx):
     if idx < len(my_list) and idx > -1:
         return my_list[idx]
     return None
+
+# def element_at(my_list, idx):
+#     if idx>len(my_list) or idx<0:
+#         return none
+# return my_list[idx]
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = [i for i in my_list]
+    # new_list= my_list.copy()
+    # new_list= my_list[:]
 
     if idx < len(new_list) and idx > -1:
         new_list[idx] = element
