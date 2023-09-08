c4073dc9d1ed438c86224c1f0856cfb85eb9e492
salmaQueen <salmawasee9@gmail.com>
2023-09-08 23:51:58 +0300

    New uupdates

--- 0x04-python-more_data_structures/1-search_replace.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    return [(my_list[i] if my_list[i] != search else replace)
-            for i in range(len(my_list))]
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    copy = a_dictionary.copy()
-    for i in copy:
-        copy[i] = copy.get(i) * 2
-
-    return copy
