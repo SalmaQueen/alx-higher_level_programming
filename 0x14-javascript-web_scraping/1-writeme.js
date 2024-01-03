be309a945c82bd2781467a986a850e455b67e278
salmaQueen <salmawasee9@gmail.com>
2024-01-03 19:03:34 +0300

    second quiz

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,22 +1,10 @@
-// Write a script that writes a string to a file.
-
-// The first argument is the file path
-// The second argument is the string to write
-// The content of the file must be written in utf-8
-// If an error occurred during while writing, print the error object
-
+#!/usr/bin/node
 const fs = require("fs");
 
-const writeMe = (file_path, text ) =>{
-
-    //we write on the filepath
-    fs.writeFile(file_path,text, "utf-8", (err)=>{
-        if(err)
-        {
+const writeMe = (file_path, text) => {
+    fs.writeFile(file_path, text, "utf-8", (err) => {
+        if (err) {
             console.error(err);
         }
-        })
-
-
+    })
 }
-writeMe(process.argv[2], process.argv[3]);
