87faefca1767331c55159c51d8d28fa099f5f14e
salmaQueen <salmawasee9@gmail.com>
2024-01-03 19:07:24 +0300

    again

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require("fs");
 
-const writeMe = (file_path, text) => {
-    fs.writeFile(file_path, text, "utf-8", (err) => {
-        if (err) {
-            console.error(err);
-        }
-    })
-}
+const [file_path, text] = process.argv.slice(2);
+fs.writeFile(file_path, text, "utf-8", (err) => {
+    if (err) {
+        console.error(err);
+    }
+})
+
