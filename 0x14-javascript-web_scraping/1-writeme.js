fe5603a3517ee31cb062ffc6282801f8290719d9
salmaQueen <salmawasee9@gmail.com>
2024-01-03 19:14:05 +0300

    more and more

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const [file_path, text] = process.argv.slice(2);
 fs.writeFile(file_path, text, "utf-8", (err) => {
     if (err) {
-        console.error(err);
+        console.error(err.message);
     }
 })
 
