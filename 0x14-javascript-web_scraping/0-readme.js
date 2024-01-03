e740c249220ffad65ed7503ee5848cfa287ffbde
salmaQueen <salmawasee9@gmail.com>
2024-01-03 18:05:44 +0300

    executable

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,23 @@
 #!/usr/bin/node
-const fs = require('fs');
+const fs = require("fs");
 
-const fileName = process.argv[2];
-fs.readFile(fileName, 'utf8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
+//check if the file exists after second argument
+
+if(process.argv.length > 3)
+{
+    console.log("The file does not exists");
+    process.exit(1);
+}
+
+else{
+   //use non-blocking(Asychronous method, readFile and not readFileSync)
+   fs.readFile(process.argv[2], "utf-8", (error, file_content)=>{
+    if(error)
+    {
+        console.error(error);
+    }
+    else{
+        console.log(file_content);
+    }
+   })
+}
